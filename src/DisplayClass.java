105536f9403444cd2f0b1e44c03d9068515a0b90
era97 <afrinjahanera03@gmail.com>
2025-03-26 19:02:51 +0600

    Bookingmanager for crud operation on booking

--- src/DisplayClass.java
+++ src/DisplayClass.java
@@ -2,7 +2,10 @@ import java.util.List;
 
 public interface DisplayClass {
     void displayRegisteredUsersForAllFlight();
+
     void displayRegisteredUsersForASpecificFlight(String flightNum);
+
     void displayHeaderForUsers(Flight flight, List<Customer> c);
+    
     void displayFlightsRegisteredByOneUser(String userID);
 }
